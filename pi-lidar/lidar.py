c8af5222e04da147e4b93db8b431df6a922d1a21
Sebastian King <sebastian.king@btinternet.com>
2019-03-17 18:27:46 +0000

    added pi-lidar code

---
+++ pi-lidar/lidar.py
@@ -0,0 +1,22 @@
+from __future__ import division, print_function
+import time
+from tfmini_library import TFmini
+
+# create the sensor and give it a port and (optional) operating mode
+tf = TFmini('/dev/ttyS0', mode=TFmini.STD_MODE)
+
+print('init done');
+
+try:
+    print('='*25)
+    while True:
+        d = tf.read()
+        if d:
+            print('Distance: {:5}, Strength: {:5}'.format(d[0], d[1]))
+        else:
+            print('No valid response')
+        time.sleep(0.1)
+
+except KeyboardInterrupt:
+    tf.close()
+    print('bye!!')
