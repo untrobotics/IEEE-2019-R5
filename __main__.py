b115f9712cd368626a9defff4692f7c9139907c0
ntindle <nicktindle@outlook.com>
2019-04-01 00:09:49 -0500

    Renamed pretty much every file

--- __main__.py
+++ __main__.py
@@ -1,1 +1,8 @@
-import 
+from pilidar import lidar
+
+def main():
+    print("Hlelo Worlkd");
+
+if __name__=="__main__":
+    main()
+
--- piusbserial/usb-serial.py
+++ piusbserial/usb-serial.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 import serial
-ser = serial.Serial('/dev/ttyACM0', 9600)
+ser = serial.Serial('/dev/ttyACM0', 115200)
 
 # read from Arduino
 input = ser.read()
