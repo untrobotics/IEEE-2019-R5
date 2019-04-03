bbc8a8eeb333a6a86fdfb5136be6d675ef77f408
woodward54 <woodwarddavid54@gmail.com>
2019-04-03 13:20:44 -0500

    classes

--- hardwareControl/hardwareControl.py
+++ hardwareControl/hardwareControl.py
@@ -1,86 +1,89 @@
-#This program accepts two int values representing the X and Y cordinate to move to as well as the rotation of the bot during the move 
+#This program accepts two int values representing the X and Y cordinate to move to as well as the rotation of the bot during the move
 
-#First get data from C++ Code 
+#First get data from C++ Code
 import time
 import math
 import smbus
 
-addr = 0x8 # bus address
-bus = smbus.SMBus(1) # indicates /dev/ic2-1
+class hardwareControl:
+    def __init__():
+        self.addr = 0x8 # bus address
+        self.bus = smbus.SMBus(1) # indicates /dev/ic2-1
 
-currentX = 500
-currentY = 500
-currentRot = 0 #in degrees
+        self.currentX = 500
+        self.currentY = 500
+        self.currentRot = 0 #in degrees
 
-destX = 501
-destY = 500
-destRot = 0 #just moving forward to start with pi code
+        self.destX = 501
+        self.destY = 500
+        self.destRot = 0 #just moving forward to start with pi code
 
-V1 = 0
-V2 = 0 
-V3 = 0
-V4 = 0
- 
-grabber_Move_Flag = False
-grabber_Run_Flag  = False
+        self.V1 = 0
+        self.V2 = 0
+        self.V3 = 0
+        self.V4 = 0
 
-Vd = .5 #Desired Speed
+        self.grabber_Move_Flag = False
+        self.grabber_Run_Flag  = False
 
-Angle = 0 
+        self.Vd = .5 #Desired Speed
 
-AngleSpeed = 0
+        self.Angle = 0
 
-#Then reference the data to get distance/difference values
-diffX = destX - currentX
-diffY = destY - currentY
-diffRot = destRot - currentRot
+        self.AngleSpeed = 0
 
-def Send(inputString):
-    bus.write_byte_data(addr, 0 , ord('<'))
-    time.sleep(.001)
-    for c in inputString:
-        bus.write_byte_data(addr, 0, ord(c))
+        #Then reference the data to get distance/difference values
+        self.diffX = destX - currentX
+        self.diffY = destY - currentY
+        self.diffRot = destRot - currentRot
+
+
+    def Send(inputString):
+        bus.write_byte_data(addr, 0 , ord('<'))
+        time.sleep(.001)
+        for c in inputString:
+            bus.write_byte_data(addr, 0, ord(c))
+            time.sleep(.001)
+
+        bus.write_byte_data(addr, 0 , ord('>'))
         time.sleep(.001)
 
-    bus.write_byte_data(addr, 0 , ord('>'))
-    time.sleep(.001)
-
-def move(x, y):  
-    #This is where the encoder calculation is going
+    def move(x, y):
+        #This is where the encoder calculation is going
 
 
 
 
 
 
-    inputString = "drive,-30,-30,-30,-30,10000"
-    Send(inputString)
-def grabberMove(logic):
-    global grabber_Move_Flag
-    if(grabber_Move_Flag):
-        inputString = "moveGrabber,2000,0"
-        grabber_Move_Flag = True  
-    else:
-        inputString = "moveGrabber,2000,1"
-        grabber_Move_Flag = False
-    Send(inputString)
+        inputString = "drive,-30,-30,-30,-30,10000"
+        Send(inputString)
+    def grabberMove(logic):
+        global grabber_Move_Flag
+        if(grabber_Move_Flag):
+            inputString = "moveGrabber,2000,0"
+            grabber_Move_Flag = True
+        else:
+            inputString = "moveGrabber,2000,1"
+            grabber_Move_Flag = False
+        Send(inputString)
 
 
-def runGrabber(logic):
-    global grabber_Run_Flag 
-    if(grabber_Run_Flag):
-        inputString = "runGrabber,2000,{}".format(logic)
-        print(inputString)
-        grabber_Run_Flag = True
-    else:
-        inputString = "runGrabber,2000,1"
-        grabber_Run_Flag = False
-    Send(inputString)
+    def runGrabber(logic):
+        global grabber_Run_Flag
+        if(grabber_Run_Flag):
+            inputString = "runGrabber,2000,{}".format(logic)
+            print(inputString)
+            grabber_Run_Flag = True
+        else:
+            inputString = "runGrabber,2000,1"
+            grabber_Run_Flag = False
+        Send(inputString)
 
 
 #Main instructions
-#Here is where we will recieve our vector of commands 
-#vector from C++ Loaded here 
-
+#Here is where we will recieve our vector of commands
+#vector from C++ Loaded here
+controller = hardwareControl()
 
 move(1,2)
