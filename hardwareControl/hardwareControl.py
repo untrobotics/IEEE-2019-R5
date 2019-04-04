94da49522b2d3cd3ce3f108800fb36a94787f466
woodward54 <woodwarddavid54@gmail.com>
2019-04-05 00:36:04 +0100

    tim push

--- hardwareControl/hardwareControl.py
+++ hardwareControl/hardwareControl.py
@@ -5,6 +5,7 @@ import time
 import math
 import smbus
 
+
 class hardwareControl:
 
     distance = 0
@@ -28,27 +29,32 @@ class hardwareControl:
     V3 = 0
     V4 = 0
 
-    encoderCorrection = 200 #correction value for the encoder
+    encoderCorrection = 155 #correction value for the encoder
 
     moveGrabberUptime = 2200 #time to move grabber up and down consistently 
     moveGrabberDowntime = 1900
-    Vd = .5 #Desired Speed
+    Vd = 100#Desired Speed
 
     Angle = 0
 
-    AngleSpeed = 0
+
+
+    AngleSpeed = 1
 
     #Then reference the data to get distance/difference values
-    diffX = destX - currentX
-    diffY = destY - currentY
-    diffRot = destRot - currentRot
 
-    def readData(self):
+    def readandMove(self):
+        print("READ")
         f = open("path.txt",'r') #opening the file pointer to read from 
-        lines = f.readlines() #reads all of the lines from file
+        #lines = f.readlines() #reads all of the lines from file
 
-        for i, line in enumerate(lines): #gets each line in file 
+        for line in f: #gets each line in file 
+            print("LOOP")
             commands = line.split(",")
+            self.rotation(self, commands[2])
+            time.sleep(.5)
+            self.drive(self, commands[0], commands[1])
+            time.sleep(.5)
 
     def send(self, inputString):
         self.bus.write_byte_data(self.addr, 0 , ord('<'))
@@ -60,31 +66,63 @@ class hardwareControl:
         self.bus.write_byte_data(self.addr, 0 , ord('>'))
         time.sleep(.001)
 
+    def rotation(self, rot):
+        self.destRot = float(rot) - self.currentRot
+        if(self.destRot >0):
+            self.V1 = 0
+            self.V2 = 0
+            self.V3 = 20
+            self.V4 = 20
+        
+        #send the command with a distance do some trig or shit
+
+
     def drive(self, x, y):
         global distance
         global encoderCorrection 
-        self.distance = math.sqrt((pow((x-self.currentX),2)) + (pow((y-self.currentY),2))) #this should give us a distance to travel
+        self.distance = math.sqrt((pow((float(x)-self.currentX),2)) + (pow((float(y)-self.currentY),2))) #this should give us a distance to travel
         self.distance *= self.encoderCorrection #change the encoder value 
+        #print("X: "+x))
+        if(float(x) != 0.0):
 
-#set v value 
 
-        self.V1 = 30  
-        self.V2 = 30
-        self.V3 = 30
-        self.V4 = 30
+            self.Angle = math.radians(math.atan(float(y)/float(x)))#i think this is right 
 
-        print ("X: {} and Y: {}".format(x,y))
+        else:
+            if(float(y) > 0):
+                self.Angle = (math.pi/2)
+            else:
+                self.Angle = (3 * math.pi)/2
+
+        
+        print((self.Angle))
+
+
+
+       # self.V1 = self.Vd*(math.cos(self.Angle - (math.pi/4)))
+        #self.V2 = self.Vd*(math.sin(self.Angle - (math.pi/4)))
+        #self.V3 = self.Vd*(math.sin(self.Angle - (math.pi/4)))
+        #self.V4 = self.Vd*(math.cos(self.Angle - (math.pi/4)))
+
+        
+        self.V1 = 90
+        self.V2 = -90
+        self.V3 = -90
+        self.V4 = 90    #print ("X: {} and Y: {}".format(x,y))
+        print("V1: {} V2: {} V3: {} V4: {}".format(self.V1,self.V2,self.V3,self.V4))
         print ("Distance: {}".format(self.distance))
-        inputString = "drive,{},{},{},{},{}".format(self.V1,self.V2,self.V3,self.V4,self.distance)
+        print("drive,{},{},{},{},{}".format(round(self.V1, 3),round(self.V2,3),round(self.V3,3),round(self.V4,3),int(round(self.distance,3))))
+
+        inputString = "drive,{},{},{},{},{}".format(round(self.V1, 3),round(self.V2,3),round(self.V3,3),round(self.V4,3),int(round(self.distance,3)))
         self.send(self,inputString)
 
-        self.currentX = x
-        self.currentY = y
+        self.currentX = float(x)
+        self.currentY = float(y)
 
     def moveGrabber(self, logic):
         global MoveFlag
         global moveGrabberUptime
-        global moveGrabberDowntime
+        global moveGrabberDowntime  
         if(logic == 1): #Moves the grabber up
             if(self.MoveFlag == 0):
                 inputString = "moveGrabber,{},1".format(self.moveGrabberUptime)
@@ -109,7 +147,11 @@ class hardwareControl:
 
         self.send(self,inputString)
 
+
+
+
     def pickupBlock(self):
+        
         #self.distance = 2000
         # get flush
         # drive X distance
@@ -125,15 +167,17 @@ class hardwareControl:
         #self.distance += .5
         #time.sleep(.5)
         #Sself.drive(self,1,1)
-#        self.runGrabber(self,1)
-
-        self.drive(self,0,20)
-
+        #self.runGrabber(self,1)
+        #time.sleep(4)
+        #self.drive(self,0,20)
+        #time.sleep(.5)
             #time.sleep(2.1)
             #self.moveGrabber(self,1)
             #time.sleep(3)
             #self.moveGrabber(self,0)
-            #self.drive(self,0,20)
+
+        self.drive(self,30,30)
+        time.sleep(2.5)
         # translate right from block detected to half block
         #for x in (0,self.distance/2, .5): #goes back half
          #   self.drive(self,-.5, 0) #drive back half the distance 
@@ -161,5 +205,6 @@ class hardwareControl:
 #vector from C++ Loaded here
 
 test = hardwareControl
-test.readData(test)
+#test.readandMove(test) #getting data from file
+
 test.pickupBlock(test)
