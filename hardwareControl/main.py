c961374d480b84b4d5bf0650ce6cfe0099c4ba4f
woodward54 <woodwarddavid54@gmail.com>
2019-04-04 01:43:47 -0500

    push

--- hardwareControl/main.py
+++ hardwareControl/main.py
@@ -3,6 +3,19 @@ from hardwareControl.hardwareControl import hardwareControl
 import time
 import math
 
+ROUND = 1
+
+if (ROUND == 1):
+    NUMOFBLOCKS = 2
+    NUMOFOBS = 5
+elif (ROUND == 2):
+    NUMOFBLOCKS = 4
+    NUMOFOBS = 10
+else:
+    NUMOFBLOCKS = 6
+    NUMOFOBS = 15
+
+
 lidarObj = lidar.lidarControl()
 
 controller = file.hardwareControl(lidarObj)
