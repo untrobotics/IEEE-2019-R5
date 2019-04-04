e00121da88a0d57f5620dcc03d69cc8eef4ced92
woodward54 <woodwarddavid54@gmail.com>
2019-04-04 02:04:41 -0500

    f

--- hardwareControl/lidar.py
+++ hardwareControl/lidar.py
@@ -29,6 +29,7 @@ class lidarControl(object):
             if recv[0] == 0x59 and recv[1] == 0x59:     #python3
                 distance = recv[2] + recv[3] * 256
                 strength = recv[4] + recv[5] * 256
+                print('Test')
                 print('(', distance, ',', strength, ')')
                 self.ser.reset_input_buffer()
 
