eae663c1fb00749d8e2b2b6f1c116774913b1d41
woodward54 <woodwarddavid54@gmail.com>
2019-04-04 18:00:33 -0500

    push

--- hardwareControl/lidar.py
+++ hardwareControl/lidar.py
@@ -19,30 +19,9 @@ class lidarControl(object):
         else:
             print('No valid response')
 
-        if self.ser != None:
-            self.ser.close()
-
-
-    # def getTFminiData(self):
-    #     count = self.ser.in_waiting
-    #     print(count)
-    #     if count > 8:
-    #         print('Test3')
-    #         recv = self.ser.read(9)
-    #         self.ser.reset_input_buffer()
-    #         # type(recv), 'str' in python2(recv[0] = 'Y'), 'bytes' in python3(recv[0] = 89)
-    #         # type(recv[0]), 'str' in python2, 'int' in python3
-    #
-    #         if recv[0] == 0x59 and recv[1] == 0x59:     #python3
-    #             distance = recv[2] + recv[3] * 256
-    #             strength = recv[4] + recv[5] * 256
-    #             print('Test')
-    #             print('(', distance, ',', strength, ')')
-    #             self.ser.reset_input_buffer()
-
 
     def __del__(self):
-        self.ser.close()
+        tf.close()
         #f.close()
 
 
