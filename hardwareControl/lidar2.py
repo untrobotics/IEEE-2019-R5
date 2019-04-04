67de7659917fefd9fe943e5397ac6dc7879cf278
woodward54 <woodwarddavid54@gmail.com>
2019-04-04 15:46:53 -0500

    push

--- hardwareControl/lidar2.py
+++ hardwareControl/lidar2.py
@@ -3,7 +3,7 @@ import time
 from tfmini import TFmini
 
 # create the sensor and give it a port and (optional) operating mode
-tf = TFmini('/dev/tty.usbserial-A506BOT5', mode=TFmini.STD_MODE)
+tf = TFmini('/dev/ttyS0', mode=TFmini.STD_MODE)
 
 try:
     print('='*25)
